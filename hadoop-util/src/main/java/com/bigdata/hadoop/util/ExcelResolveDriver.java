4271a421993fb8728c619d481717552d972c44b1
wangbin <12345678>
2017-08-28 11:32:41 +0800

    添加开发环境相关配置

--- hadoop-util/src/main/java/com/bigdata/hadoop/util/ExcelResolveDriver.java
+++ hadoop-util/src/main/java/com/bigdata/hadoop/util/ExcelResolveDriver.java
@@ -13,6 +13,8 @@
  */
 package com.bigdata.hadoop.util;
 import com.bigdata.hadoop.util.mapreduce.ExcelConfig;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.util.ToolRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,8 +38,7 @@ public class ExcelResolveDriver {
 	public static void main(String[] args) throws Exception {		
 		//判断参数个数是否正确
 		//如果无参数运行则显示以作程序说明
-		if ( args.length != 2 )
-		{
+		if ( args.length != 2 ) {
 			System.err.println("");
 			System.err.println("Usage:ExcelResolveDriver < input path > < output path > ");
 			System.err.println("Example: hadoop jar ~/ExcelResolveDriver input out");
@@ -50,8 +51,15 @@ public class ExcelResolveDriver {
 		Date start = new Date();
 
 		ExcelConfig conf = new ExcelConfig();
+		System.setProperty("hadoop.home.dir", "D:\\hadoop-workspace\\hadoop-2.8");
+		JobConf c = new JobConf();
+		c.set("fs.defaultFS", "hdfs://master.hadoop:9000");
+		c.set("mapreduce.app-submission.cross-platform", "true");
+		c.set("mapreduce.framework.name", "yarn");
+		c.set("mapreduce.job.jar","D:\\hadoop-workspace\\hadoop\\hadoop-util-jar-with-dependencies.jar");
+		c.setJar("D:\\hadoop-workspace\\hadoop\\hadoop-util\\target\\hadoop-util-jar-with-dependencies.jar");
 		//运行任务
-		int res = ToolRunner.run(conf.getConf(),conf,args);
+		int res = ToolRunner.run(c,conf,args);
 
 		//输出任务耗时
 		Date end = new Date();
